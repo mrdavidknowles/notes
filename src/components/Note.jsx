22fa2a9a0c742083f4c069e6a08f3f5728898460
David Knowles <dknowles86@gmail.com>
2020-03-04 19:40:56 -0700

    Add note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className = "note">
+            <h1>Note Heading</h1>
+            <p>Note content</p>
+        </div>
+    )
+}
+
+export default Note;
