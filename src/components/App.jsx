6ec83cd76b2df6d5ade2a3c167493f31c329474e
David Knowles <dknowles86@gmail.com>
2020-03-04 19:21:10 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header.jsx"
+import Footer from "./Footer.jsx"
+
+function App() {
+    return (
+    <div>
+        <Header />
+        <Footer />
+    </div>
+    )
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+let currentYear = new Date().getFullYear();
+
+function Footer() {
+    return (
+        <footer><p>Copyright &copy; {currentYear}</p></footer>
+    )
+}
+
+export default Footer;
