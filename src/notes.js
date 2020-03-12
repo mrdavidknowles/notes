9dccd950c31a9824ea3ab699b060ce276b1eb499
David Knowles <dknowles86@gmail.com>
2020-03-11 21:16:26 -0600

    Add props and map function to display multiple note objects from array

---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+    {
+      key: 1,
+      title: "Shopping List",
+      content:
+        "Apples, bananas, carrots, dill pickles"
+    },
+    {
+      key: 2,
+      title: "Lunch @ 1PM",
+      content:
+        "Meet John for lunch tomorrow at Chipotle"
+    },
+    {
+      key: 3,
+      title: "Schedule vacation",
+      content:
+        "Schedule vacation trip to the moon!"
+    },
+    {
+      key: 4,
+      title: "Where is the remote?",
+      content:
+        "Looked everywhere, not under couch or in chair, must continue..."
+    }
+  ];
+
+  export default notes;
+  
